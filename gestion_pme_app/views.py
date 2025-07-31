8570a33043e2e9fa35921ffff6e8eb2fa9dfd8da
rhhtahina <rhhtahina@gmail.com>
2025-07-31 20:46:58 +0300

    login message

--- gestion_pme_app/views.py
+++ gestion_pme_app/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 def home(request):
-    return render(request, 'home.html')
+    # check to sse if logging in
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You Have Been Logged In!')
+            return redirect('home')
+        else:
+            messages.error(request, 'There Was An Error Logging In, Please Try Again...')
+            return redirect('home')
+    else:
+        return render(request, 'home.html')
 
 def logout_user(request):
     pass
