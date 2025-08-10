8ccc91c7b67306e581259a8bc4edf8b29ae16cdd
rhhtahina <rhhtahina@gmail.com>
2025-08-10 20:58:05 +0300

    register

--- gestion_pme_app/views.py
+++ gestion_pme_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 def home(request):
     # check to sse if logging in
@@ -21,4 +22,22 @@ def home(request):
         return render(request, 'home.html')
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, 'You Have Been Logged Out!')
+    return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'you Have Successful Registered! Welcome!')
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
